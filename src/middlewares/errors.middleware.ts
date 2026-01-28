b46e09721a1a37644a22ece791502bdacc74e804
HossamGezo <ha2ghossam10@gmail.com>
2026-01-28 10:30:24 +0200

    feat: implement auth middlewares and global types

--- src/middlewares/errors.middleware.ts
+++ src/middlewares/errors.middleware.ts
@@ -13,16 +13,28 @@ export const notFound = (req: Request, res: Response, next: NextFunction) => {
  * @desc Global Error Handler
  */
 export const errorHandler = (
-  err: unknown,
+  err: any,
   _req: Request,
   res: Response,
   _next: NextFunction,
 ) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  const message = err instanceof Error ? err.message : "Something went wrong!";
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message || "Something went wrong!";
+
+  // --- MongoDB Bad ObjectId (CastError)
+  if (err.name === "CastError") {
+    statusCode = 400;
+    message = "Invalid ID Format";
+  }
+
+  // --- MongoDB Duplicate Key Error (e.g. Email exists)
+  if (err.code === 11000) {
+    statusCode = 400;
+    message = "Duplicate field value entered";
+  }
 
   res.status(statusCode).json({
     message: message,
-    stack: process.env.NODE_ENV === "production" ? null : (err as Error).stack,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
