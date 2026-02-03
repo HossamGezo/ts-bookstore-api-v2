32973a9a3294dc0c7b1c48c5161499dd174c7f94
HossamGezo <ha2ghossam10@gmail.com>
2026-02-04 01:11:11 +0200

    feat: implement forgot-password MVC feature with EJS, Tailwind CSS and Nodemailer

---
+++ src/controllers/password.controller.ts
@@ -0,0 +1,133 @@
+// --- Libraries
+import type {Request, Response} from "express";
+import asyncHandler from "express-async-handler";
+
+// --- Validations
+import {
+  validateEmail,
+  validatePassword,
+} from "../validations/password.validation.js";
+
+// --- Services
+import {
+  getResetPasswordViewService,
+  resetPasswordService,
+  sendForgotPasswordLinkService,
+} from "../services/password.service.js";
+
+// --- Types
+type ServiceResponse = {
+  success: boolean;
+  statusCode?: number;
+  message?: string;
+  data?: any;
+};
+
+// --- HTTP Methods (Verbs)
+
+/**
+ * @desc Get Forgot Password View
+ * @route /password/forgot-password
+ * @method GET
+ * @access public
+ */
+export const getForgotPasswordView = asyncHandler(
+  async (_req: Request, res: Response) => {
+    res.render("forgot-password");
+  },
+);
+
+/**
+ * @desc Send Forgot Password Link
+ * @route /password/forgot-password
+ * @method POST
+ * @access public
+ */
+export const sendForgotPasswordLink = asyncHandler(
+  async (req: Request, res: Response) => {
+    // --- Validation
+    const validation = validateEmail(req.body);
+    if (!validation.success) {
+      res.status(400).json({message: validation.error.issues[0]?.message});
+      return;
+    }
+
+    // --- Service
+    const result = (await sendForgotPasswordLinkService(
+      validation.data,
+    )) as ServiceResponse;
+
+    // --- Failure
+    if (!result.success) {
+      res.status(result.statusCode!).json({message: result.message});
+      return;
+    }
+
+    // --- Response
+    res.render("link-send");
+  },
+);
+
+/**
+ * @desc Get Reset Password View
+ * @route /password/reset-password/:id/:token
+ * @method GET
+ * @access public
+ */
+export const getResetPasswordView = asyncHandler(
+  async (req: Request, res: Response) => {
+    // --- Invalid
+    if (!req.params.id && !req.params.token) {
+      res.status(400).json({message: "Invalid Request!"});
+      return;
+    }
+
+    // --- Service
+    const result = (await getResetPasswordViewService(
+      req.params.id!,
+      req.params.token!,
+    )) as ServiceResponse;
+
+    // --- Failure
+    if (!result.success) {
+      res.status(result.statusCode!).json({message: result.message});
+      return;
+    }
+
+    // --- Response
+    res.render("reset-password", {email: result.data.email});
+  },
+);
+
+/**
+ * @desc Reset Password
+ * @route /password/reset-password/:id/:token
+ * @method POST
+ * @access public
+ */
+export const resetPassword = asyncHandler(
+  async (req: Request, res: Response) => {
+    // --- Validation
+    const validation = validatePassword(req.body);
+    if (!validation.success) {
+      res.status(400).json({message: validation.error.issues[0]?.message});
+      return;
+    }
+
+    // --- Service
+    const result = (await resetPasswordService(
+      req.params.id!,
+      req.params.token!,
+      validation.data.password,
+    )) as ServiceResponse;
+
+    // --- Failure
+    if (!result.success) {
+      res.status(result.statusCode!).json({message: result.message});
+      return;
+    }
+
+    // --- Response
+    res.render("success-password");
+  },
+);
